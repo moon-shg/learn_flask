7ca8fee3c3d2c18f42134267757fa09861312d6e
Mr.mooon <393773661@qq.com>
2019-11-07 16:59:24 +0800

    finish 8.4.7

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length, Email
 
 
 class LoginForm(FlaskForm):
-	email = StringField('Email', validators=[DataRequired(), Length(1,64), Email])
+	email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
 	password = PasswordField('password', validators=[DataRequired()])
 	remember_me = BooleanField('保持登录状态')
 	submit = SubmitField('登录')
