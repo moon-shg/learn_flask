53ad0d2df702787b72c9f89e4a5f4120762a4c5d
Mr.mooon <393773661@qq.com>
2019-11-07 14:57:47 +0800

    fixed some bugs in 8.4.3

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from . import auth
+from .forms import LoginForm
 
 
 @auth.route('/login')
 def login():
-	return render_template('auth/login.html')
+	form = LoginForm()
+	return render_template('auth/login.html', form=form)
