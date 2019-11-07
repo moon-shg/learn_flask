3c67792b2763f339c2646877541b00224e503fb0
Mr.mooon <393773661@qq.com>
2019-11-07 19:54:30 +0800

    finish 8.5

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +8,21 @@ class LoginForm(FlaskForm):
 	password = PasswordField('password', validators=[DataRequired()])
 	remember_me = BooleanField('保持登录状态')
 	submit = SubmitField('登录')
+
+class RegistrationForm(FlaskForm):
+	email = StringField('邮箱', validators=[DataRequired(), Length(1,64), Email()])
+	username = StringField('用户名', validators=[DataRequired(), Length(1,64), 
+		Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 
+		'用户名必须由大小写英文字母，数字，小数点或下划线构成')])
+	password = PasswordField('密码', validators=[
+		DataRequired(), EqualTo('password2', message='两次密码不相同')])
+	password2 = PasswordField('确认密码', validators=[DataRequired()])
+	submit = SubmitField('注册')
+
+def validate_email(self, field):
+	if User.query.filter_by(email=field.data).first():
+		raise ValidationError('该邮箱已注册！')
+
+def validate_username(self, field):
+	if User.query.filter_by(Username=field.data).first():
+		raise ValidationError('用户名已被使用！')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, request, url_for, redirect, flash
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User
 from flask_login import login_user, logout_user, login_required
+from app import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -27,3 +28,14 @@ def logout():
 	flash('已退出登录！')
 	return redirect(url_for('main.index'))
 	
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('已成功注册，你现在可以登录了！')
+		return redirect(url_for('auth.login'))
+	return render_template('auth/register.html', form=form)
