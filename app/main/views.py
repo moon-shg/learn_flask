9b02c75e84544c8ad6668279a7e93df13705b3a5
Mr.mooon <393773661@qq.com>
2019-11-13 16:13:36 +0800

    add test_api.py

--- app/main/views.py
+++ app/main/views.py
@@ -251,3 +251,14 @@ def moderate_disable(id):
 	db.session.add(comment)
 	db.session.commit()
 	return redirect(url_for('.moderate', page=request.args.get('page', 1, type=int)))
+
+# 关闭服务器的路由
+@main.route('/shutdown')
+def server_shutdown():
+	if not current_app.testing:
+		abort(403)
+	shutdown = request.environ.get('werkzeug.server.shutdown')
+	if not shutdown:
+		abort(500)
+	shutdown()
+	return 'Shutting down...'
